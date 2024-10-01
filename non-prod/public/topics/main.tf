851ae57542bd139d1f00a4d6d78e67e65c009f39
Niyi Odumosu <35928853+NiyiOdumosu@users.noreply.github.com>
2024-10-01 15:09:56 +0000

    Added topic a

--- non-prod/public/topics/main.tf
+++ non-prod/public/topics/main.tf
@@ -13,18 +13,3 @@
      secret = confluent_api_key.producer-api-key.secret
    }
  }
- resource "confluent_kafka_topic" "topic_b" {
-   kafka_cluster {
-     id = confluent_kafka_cluster.basic.id
-   }
-   topic_name         = "topic_b"
-   partitions_count   = 3
-   rest_endpoint      = confluent_kafka_cluster.basic.rest_endpoint
-   config = {
-
-   }
-   credentials {
-     key    = confluent_api_key.producer-api-key.id
-     secret = confluent_api_key.producer-api-key.secret
-   }
- }
