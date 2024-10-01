5057d1944bf750d9b6ed359f1cf8306d82061c69
Niyi Odumosu <35928853+NiyiOdumosu@users.noreply.github.com>
2024-10-01 15:28:40 -0400

    Added two new topics

--- non-prod/public/topics/main.tf
+++ non-prod/public/topics/main.tf
@@ -12,4 +12,34 @@
      key    = confluent_api_key.producer-api-key.id
      secret = confluent_api_key.producer-api-key.secret
    }
+
+  resource "confluent_kafka_topic" "topic_b" {
+   kafka_cluster {
+     id = confluent_kafka_cluster.basic.id
+   }
+   topic_name         = "topic_a"
+   partitions_count   = 3
+   rest_endpoint      = confluent_kafka_cluster.basic.rest_endpoint
+   config = {
+
+   }
+   credentials {
+     key    = confluent_api_key.producer-api-key.id
+     secret = confluent_api_key.producer-api-key.secret
+   }
+
+ resource "confluent_kafka_topic" "topic_c" {
+   kafka_cluster {
+     id = confluent_kafka_cluster.basic.id
+   }
+   topic_name         = "topic_a"
+   partitions_count   = 3
+   rest_endpoint      = confluent_kafka_cluster.basic.rest_endpoint
+   config = {
+
+   }
+   credentials {
+     key    = confluent_api_key.producer-api-key.id
+     secret = confluent_api_key.producer-api-key.secret
+   }
  }
